249459d05d94d6470deead386e451bd01d2b0fd4
mattruma <mattruma@gmail.com>
2020-07-02 15:47:51 -0400

    inital commit

---
+++ BlazorApp1/Helpers/JSRuntimeExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Helpers
+{
+    public static class JSRuntimeExtensions
+    {
+        public async static Task ClickAsync(
+            this IJSRuntime jsRuntime,
+            string element)
+        {
+            await jsRuntime.InvokeAsync<object>(
+                  "click",
+                  element);
+        }
+
+        public async static Task SaveAsAsync(
+            this IJSRuntime jsRuntime,
+            string filename,
+            byte[] data)
+        {
+            await jsRuntime.InvokeAsync<object>(
+                  "saveAsFile",
+                  filename,
+                  Convert.ToBase64String(data));
+        }
+    }
+}
